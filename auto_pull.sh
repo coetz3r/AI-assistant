896e3fdd0f4816e12534b28343a306ec76dc633e
coet3r <coetz3r@gmail.com>
2026-08-20 13:27:44 +0200

    fixed bug (removed chmod) and cleaned code

--- auto_pull.sh
+++ auto_pull.sh
@@ -1,4 +1,3 @@
-cat << 'EOF' > auto_pull.sh
 #!/bin/bash
 set -e
 cd "$(dirname "$0")"
@@ -8,7 +7,5 @@ LOCAL=$(git rev-parse HEAD)
 REMOTE=$(git rev-parse origin/main)
 
 if [ "$LOCAL" != "$REMOTE" ]; then
-   git pull origin main
+    git pull origin main
 fi
-EOF
-chmod +x auto_pull.sh
